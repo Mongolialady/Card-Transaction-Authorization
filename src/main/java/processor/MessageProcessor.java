86ed7ae16a2127e45ed588cc11ce135004f941bb
Mongolialady <hhy615@gmail.com>
2021-04-17 05:54:00 -0700

    save before continue

--- src/main/java/processor/MessageProcessor.java
+++ src/main/java/processor/MessageProcessor.java
@@ -37,8 +37,13 @@ public class MessageProcessor {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("YYMM");
         LocalDate localDate = LocalDate.now();
         String dt = localDate.format(dtf);
-        String ed = txnMessage.getExpirationDate().substring(2) + txnMessage.getExpirationDate().substring(0, 2);
+        String yy = txnMessage.getExpirationDate().substring(2);
+        String mm = txnMessage.getExpirationDate().substring(0, 2);
+        if(Integer.parseInt(mm) > 12){
+            return false;
+        }
 
+        String ed = yy + mm;
         return ed.compareTo(dt) > 0;
     }
 
@@ -46,39 +51,44 @@ public class MessageProcessor {
         TxnMessage msg = new TxnMessage();
         int j = 0;
         // message type indicator
-        if (j < message.length()) {
+        if (j < message.length() && (j + 4) < message.length()) {
             msg.setMsgTypeInd(message.substring(j, j += 4));
         }
 
         // bitMap
-        if (j < message.length()) {
+        if (j < message.length() && (j + 2) < message.length()) {
             msg.setBitMap(message.substring(j, j += 2));
         }
 
         // pan number
-        if (j < message.length()) {
+        if (j < message.length() && (j + 2) < message.length()) {
             String x = message.substring(j, j + 2);
             int y = Character.getNumericValue(x.charAt(0)) * 10 + Character.getNumericValue(x.charAt(1));
-            msg.setPanNum(message.substring(j, j += y + 2));
+            if((j + y + 2) < message.length()) {
+                msg.setPanNum(message.substring(j, j += y + 2));
+            }
         }
 
         // expiration date
-        if (j < message.length()) {
+        if (j < message.length() && (j + 4) < message.length()) {
             msg.setExpirationDate(message.substring(j, j += 4));
         }
 
         // transaction amount
-        if (j < message.length()) {
+        String ss = message.substring(j, j + 10);
+        if (j < message.length() && (j + 10) <= message.length()) {
             msg.setTxnAmount(message.substring(j, j += 10));
         }
 
-        if (j < message.length()) {
+        if (j < message.length() && (j + 2) < message.length()) {
             String x = message.substring(j, j + 2);
             int y = Character.getNumericValue(x.charAt(0)) * 10 + Character.getNumericValue(x.charAt(1));
-            msg.setCardholderName(message.substring(j, j += y + 2));
+            if((j + y + 2) < message.length()) {
+                msg.setCardholderName(message.substring(j, j += y + 2));
+            }
         }
 
-        if (j < message.length()) {
+        if (j < message.length() && (j + 5) == message.length()) {
             msg.setZipCode(message.substring(j));
         }
 
